ce299902825048ede7d78038a0c50a4d845c7bf0
luis <luisnavarroFS@gmail.com>
2024-03-18 21:52:49 +0100

    room occupancy percentage

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 class Room {
 
     constructor({name, rate, discount}) {
@@ -16,7 +17,12 @@ class Room {
     }   
 
     occupancyPercentage(startDate, endDate) {
-
+        const rangeDates = getDatesInRange(startDate, endDate)
+        let occupiedDays = 0
+        rangeDates.map(date => this.isOccupied(date)).forEach(date => {
+            if (date) occupiedDays++
+        })
+        return Math.trunc((occupiedDays / rangeDates.length) *100)
     }
 
     totalOccupancyPercentage(rooms, startDate, endDate) {
@@ -41,6 +47,22 @@ class Booking {
     getFee() {
 
     }
+}
+
+function getDatesInRange(startDate, endDate) {
+    const start = new Date(new Date(startDate))
+    const end = new Date(new Date(endDate))
+    
+ const date = new Date(start.getTime())
+
+ const dates = [];
+
+ while (date <= end) {
+  dates.push(new Date(date).toISOString().slice(0,10));
+  date.setDate(date.getDate() + 1);
+ }
+
+ return dates;
 }
 
 module.exports = {Room, Booking}
