ade23e1c39d84e21507216a0b0b1ebcec253d1e1
luis <luisnavarroFS@gmail.com>
2024-03-19 18:03:27 +0100

    feat: getFee method

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Room {
         this.name = name
         this.bookings = []
         this.discount = discount
-        this.rate = rate * (1 - this.discount/100) * 100
+        this.rate = rate
     }
 
     isOccupied(date) {
@@ -44,17 +44,22 @@ class Room {
 }
 
 class Booking {
-    constructor({name, email, checkin, checkout, discount}) {
+    constructor({name, email, checkin, checkout, discount, room}) {
         this.name = name
         this.email = email
         this.checkin = checkin
         this.checkout = checkout
         this.discount = discount
-        this.room = {}
+        this.room = room
     }
 
     getFee() {
-
+        const ONE_DAY_MS = 24 * 60 * 60 * 1000; 
+        const CONVERT_TO_CENTS = 100
+        const roomFinalPricePerDay = this.room.rate  * (1 - this.room.discount/100) * CONVERT_TO_CENTS
+        const bookingLength = Math.round(Math.abs((new Date(this.checkin) - new Date(this.checkout)) / ONE_DAY_MS));
+        if (this.discount > 100 || this.room.discount > 100) throw new Error ('Discount not allowed')
+        return Math.round(roomFinalPricePerDay * bookingLength * (1 - this.discount/100)) 
     }
 }
 
@@ -62,16 +67,16 @@ function getDatesInRange(startDate, endDate) {
     const start = new Date(new Date(startDate))
     const end = new Date(new Date(endDate))
     
- const date = new Date(start.getTime())
+    const date = new Date(start.getTime())
 
- const dates = [];
+    const dates = [];
 
- while (date <= end) {
-  dates.push(new Date(date).toISOString().slice(0,10));
-  date.setDate(date.getDate() + 1);
- }
+    while (date <= end) {
+    dates.push(new Date(date).toISOString().slice(0,10));
+    date.setDate(date.getDate() + 1);
+    }
 
- return dates;
+    return dates;
 }
 
 module.exports = {Room, Booking}
